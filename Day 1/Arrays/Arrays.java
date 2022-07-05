6151b2c1a93ddd95edd6ee83cb48ef6317ef8510
Alex Zhang <sdscalexz2@gmail.com>
2022-07-05 00:47:54 -0700

    Finished some tasks

---
+++ Day 1/Arrays/Arrays.java
@@ -0,0 +1,11 @@
+package Day 1.Arrays;
+
+public class Arrays {
+    public static void.main
+        String[] names ={"Technology","ROHIN","Team Optix","Joesph Robinette Biden Jr."};
+        system.out.println(names[3]);
+        system.out.println(names[2]);
+        system.out.println(names[1]);
+        system.out.println(names[0])
+
+}
---
+++ Day 1/Variables/Variables.java
@@ -0,0 +1,10 @@
+package Day 1.Variables;
+
+public class Variables {
+int age = 15;
+int height = 6;
+char firstletter = 'a';
+String lastname = "Zhang";
+boolean programmingbest = true
+
+}
