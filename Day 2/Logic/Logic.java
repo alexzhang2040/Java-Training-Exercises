63ba52f5bf2c84e989313eecd0e5e5cb13652d54
Alex Zhang <sdscalexz2@gmail.com>
2022-07-06 18:45:36 -0700

    Finally finished Day 2 🏀🏀🏀

--- Day 2/Logic/Logic.java
+++ Day 2/Logic/Logic.java
@@ -2,24 +2,24 @@ public class Logic {
     public static void main(String[] args) {
     
     //integer list
-    int result;
+    int presult;
     int tea = 2;
     int candy = 4;
     
     //code
-        if (tea < 5 || candy < 5) {result = 0;
+        if (tea < 5 || candy < 5) {presult = 0;
         }
-        else if (tea * 2 == candy || candy * 2 == tea) {result = 2;
+        else if (tea * 2 == candy || candy * 2 == tea) {presult = 2;
         } 
-        else{result = 1;
+        else{presult = 1;
         }
-        System.out.println(result);
+        System.out.println(presult);
     
         // another part of code to display text with numbers, just wanted to test out
 
-        if (result == 0) {System.out.println("Bad Party");
+        if (presult == 0) {System.out.println("Bad Party");
         }
-        else if (result == 2) {System.out.println("Great Party");
+        else if (presult == 2) {System.out.println("Great Party");
         }
         else {System.out.println("Good Party");
         }
--- Day 2/Void Methods/VoidMethods.java
+++ Day 2/Void Methods/VoidMethods.java
@@ -1,5 +1,11 @@
 public class VoidMethods {
+    static void largernumber(double num){
+        if (num > 10){
+            System.out.println(num);
+        }
+    }
     public static void main(String[] args) {
-        //Finishing later
+        largernumber(0.37);
+        largernumber(49);
     }
 }
